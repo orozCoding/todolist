60d88b3e26c70c8dc81c5b359992003579dba6e0
orozcoding <orozcoding@gmail.com>
2022-01-11 08:18:21 -0400

    add pointer effecto

--- src/index.js
+++ src/index.js
@@ -1,51 +1,50 @@
-import _ from 'lodash';
 import './style.css';
 import Dots from './dots.svg';
 import Enter from './enter.svg';
 import Load from './load.svg';
 
 const main = document.getElementById('main');
-const listContainer= document.createElement('div')
+const listContainer = document.createElement('div');
 listContainer.id = 'list-container';
 listContainer.className = 'd-flex';
-listContainer.innerHTML = `<div id="list-title" class="d-flex row"><h3>Today's To Do</h3><img src="${Load}" alt="Load Icon"></div>
-<div id="list-input" class="d-flex row"><input type="text" id="input-field" placeholder="Add to your list..."><img src="${Enter}" alt="Enter Icon"></div>
+listContainer.innerHTML = `<div id="list-title" class="d-flex row"><h3>Today's To Do</h3><img src="${Load}" alt="Load Icon" class="click"></div>
+<div id="list-input" class="d-flex row"><input type="text" id="input-field" placeholder="Add to your list..."><img src="${Enter}" alt="Enter Icon" class="click"></div>
 <ul id="list" class="d-flex"></ul>
 <div id="list-bottom">
-    <p id="list-clear">Clear all completed</p>
-</div>`
+    <p id="list-clear" class="click">Clear all completed</p>
+</div>`;
 main.appendChild(listContainer);
 
 const list = [{
   description: 'Finish the project',
   completed: false,
-  index: 3
+  index: 3,
 },
 {
   description: 'Buy dinner',
   completed: false,
-  index: 1
+  index: 1,
 },
 {
   description: 'Walk the dog',
   completed: false,
-  index: 2
+  index: 2,
 },
 {
   description: 'Help mom with the TV',
   completed: false,
-  index: 4
-}]
+  index: 4,
+}];
 
 window.addEventListener('load', () => {
   const listUl = document.getElementById('list');
   list.sort((a, b) => a.index - b.index);
-  list.forEach(task => {
+  list.forEach((task) => {
     const newTask = document.createElement('li');
     newTask.classList.add('task-container', 'd-flex', 'row');
     newTask.innerHTML = `<input type="checkbox" class="task-cb">
     <p class="task-info">${task.description}</p>
-    <img src="${Dots}" alt="Load Icon">`
+    <img src="${Dots}" alt="Load Icon" class="click">`;
     listUl.appendChild(newTask);
   });
 });
