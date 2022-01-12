bcc396f712695059ac2c7f3c5297d36719124b2f
orozcoding <orozcoding@gmail.com>
2022-01-11 21:30:18 -0400

    fix linters

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import Trash from './trash.svg';
 import Enter from './enter.svg';
 import Load from './load.svg';
 import Check from './check.svg';
-import { main, listContainer, checkIndex, tasks, saveTaskArr, addNewTask, tasksChecker, renderTask } from './functions.js';
+import { main, listContainer, checkIndex, tasks, saveTaskArr, addNewTask, tasksChecker, renderTask } from './functions.js'; // eslint-disable-line
 
 listContainer.innerHTML = `<div id="list-title" class="d-flex row"><h3>Today's To Do</h3><img src="${Load}" alt="Load Icon" class="click"></div>
 <div id="list-input" class="d-flex row"><input type="text" id="input-field" placeholder="Add to your list..."><img id="btn-enter" src="${Enter}" alt="Enter Icon" class="click"></div>
@@ -24,21 +24,17 @@ const taskInput = document.getElementById('input-field');
 const enterBtn = document.getElementById('btn-enter');
 
 taskInput.addEventListener('keydown', (e) => {
-  if(e.key === 'Enter'){
-    if(taskInput.value === ''){
-      return;
-    } else {
-    addNewTask(taskInput, Trash, Check);
+  if (e.key === 'Enter') {
+    if (taskInput.value !== '') {
+      addNewTask(taskInput, Trash);
     }
   }
 });
 
 enterBtn.addEventListener('click', () => {
-  if(taskInput.value === ''){
-    return;
-  } else {
-  addNewTask(taskInput, Trash)
-}
+  if (taskInput.value !== '') {
+    addNewTask(taskInput, Trash);
+  }
 });
 
 const listInput = document.getElementById('list-input');
