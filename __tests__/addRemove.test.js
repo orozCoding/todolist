8673be8163b0bfe31289ef8538166286e0292073
matovu-farid <matovu90@gmail.com>
2022-01-20 13:56:52 +0300

    Complete AddTask

--- __tests__/addRemove.test.js
+++ __tests__/addRemove.test.js
@@ -1,4 +1,6 @@
-import { html, tasks } from '../test_env/html';
+import { addNewTask, removeTask } from '../test_env/addRemove';
+import { html, icons,tasks } from '../test_env/html';
+import { addTask } from '../src/theFunctions';
 
 localStorage.setItem('taskArr', JSON.stringify(tasks));
 document.body.innerHTML = html;
@@ -7,5 +9,20 @@ describe('testing addTask', () => {
   test('should recognize tasks', () => {
     const tasksList = document.querySelectorAll('#list li');
     expect(tasksList).toHaveLength(3);
+  });
+  test('should add the task 1', () => {
+    const taskInput = document.getElementById('input-field');
+    taskInput.value = 'Forth task';
+    addNewTask(taskInput, icons, addTask);
+    const tasksList = document.querySelectorAll('#list li');
+    expect(tasksList).toHaveLength(4);
+  });
+
+  test('should add the task 2', () => {
+    const taskInput = document.getElementById('input-field');
+    taskInput.value = 'Fifth task';
+    addNewTask(taskInput, icons, addTask);
+    const tasksList = document.querySelectorAll('#list li');
+    expect(tasksList).toHaveLength(5);
   });
 });
